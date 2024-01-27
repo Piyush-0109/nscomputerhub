be16cb390f7ec11292e184acc4b79d8bab03476b
Mahesh Pawar <maheshpawarseveteen@gmail.com>
2024-01-27 16:12:45 +0530

    Contact Page changes done

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,7 +1,100 @@
+import { Container, Grid } from "@mui/material"
+import Avatar from '@mui/material/Avatar';
+import Button from '@mui/material/Button';
+import CssBaseline from '@mui/material/CssBaseline';
+import TextField from '@mui/material/TextField';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Link from '@mui/material/Link';
+import Box from '@mui/material/Box';
+import { deepOrange } from '@mui/material/colors';
+import Typography from '@mui/material/Typography';
+import "./contact.css";
+
 const Contact = () => {
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const data = new FormData(event.currentTarget);
+        console.log({
+            email: data.get('email'),
+            contact: data.get('contact'),
+            fullName: data.get('fullName'),
+            query: data.get('query'),
+        });
+    };
     return (
-        <div>
-            <span>Contact</span>
+        <div className="contact-container">
+            <Container component="main" maxWidth="xs">
+                <Box
+                    sx={{
+                        marginTop: 8,
+                        display: 'flex',
+                        flexDirection: 'column',
+                        alignItems: 'center',
+                    }}
+                >
+                    {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar> */}
+                    <Avatar sx={{ bgcolor: deepOrange[500], mb: 1 }}>NS</Avatar>
+                    <Typography component="h1" variant="h5">
+                        Let's Talk
+                    </Typography>
+                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="fullName"
+                            label="Full Name"
+                            name="fullName"
+                            type="text"
+                            autoComplete="fullName"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            id="email"
+                            label="Email Address"
+                            name="email"
+                            autoComplete="email"
+                            autoFocus
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="contact"
+                            label="Contact"
+                            type="number"
+                            id="contact"
+                            autoComplete=""
+                        />
+                        <TextField
+                            margin="normal"
+                            required
+                            fullWidth
+                            name="query"
+                            label="Query"
+                            type="text"
+                            multiline
+                            rows={4}
+                            id="query"
+                            autoComplete=""
+                        />
+                        <Button
+                            type="submit"
+                            fullWidth
+                            variant="contained"
+                            sx={{ mt: 3, mb: 2 }}
+                        >
+                            Submit
+                        </Button>
+                    </Box>
+                </Box>
+            </Container>
         </div>
     )
 }
