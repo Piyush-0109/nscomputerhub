c43f869da7112caed51eff040d73fa243cf1bff0
Piyush Thakare <piyush.t0109@gmail.com>
2024-01-29 19:14:08 +0530

    Fixed the contant page styling

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -12,91 +12,88 @@ import Typography from '@mui/material/Typography';
 import "./contact.css";
 
 const Contact = () => {
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        const data = new FormData(event.currentTarget);
-        console.log({
-            email: data.get('email'),
-            contact: data.get('contact'),
-            fullName: data.get('fullName'),
-            query: data.get('query'),
-        });
-    };
-    return (
-        <div className="contact-container">
-            <Container component="main" maxWidth="xs">
-                <Box
-                    sx={{
-                        marginTop: 8,
-                        display: 'flex',
-                        flexDirection: 'column',
-                        alignItems: 'center',
-                    }}
-                >
-                    {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const data = new FormData(event.currentTarget);
+    console.log({
+      email: data.get('email'),
+      contact: data.get('contact'),
+      fullName: data.get('fullName'),
+      query: data.get('query'),
+    });
+  };
+  return (
+    <Container component="main" maxWidth="xs">
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        {/* <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
               <LockOutlinedIcon />
             </Avatar> */}
-                    <Avatar sx={{ bgcolor: deepOrange[500], mb: 1 }}>NS</Avatar>
-                    <Typography component="h1" variant="h5">
-                        Let's Talk
-                    </Typography>
-                    <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="fullName"
-                            label="Full Name"
-                            name="fullName"
-                            type="text"
-                            autoComplete="fullName"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            id="email"
-                            label="Email Address"
-                            name="email"
-                            autoComplete="email"
-                            autoFocus
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="contact"
-                            label="Contact"
-                            type="number"
-                            id="contact"
-                            autoComplete=""
-                        />
-                        <TextField
-                            margin="normal"
-                            required
-                            fullWidth
-                            name="query"
-                            label="Query"
-                            type="text"
-                            multiline
-                            rows={4}
-                            id="query"
-                            autoComplete=""
-                        />
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            sx={{ mt: 3, mb: 2 }}
-                        >
-                            Submit
-                        </Button>
-                    </Box>
-                </Box>
-            </Container>
-        </div>
-    )
+        <Avatar sx={{ bgcolor: deepOrange[500], mb: 1 }}>NS</Avatar>
+        <Typography component="h1" variant="h5">
+          Let's Talk
+        </Typography>
+        <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="fullName"
+            label="Full Name"
+            name="fullName"
+            type="text"
+            autoComplete="fullName"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            id="email"
+            label="Email Address"
+            name="email"
+            autoComplete="email"
+            autoFocus
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="contact"
+            label="Contact"
+            type="number"
+            id="contact"
+            autoComplete=""
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            name="query"
+            label="Query"
+            type="text"
+            multiline
+            rows={4}
+            id="query"
+            autoComplete=""
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Submit
+          </Button>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Contact;
