226f2dfc71722d41f3b75097958159f8e5bcd872
Piyush Thakare <piyush.t0109@gmail.com>
2024-02-01 15:46:31 +0530

    Added google map in about us page

--- src/components/About/index.jsx
+++ src/components/About/index.jsx
@@ -1,8 +1,20 @@
 const About = () => {
+
     return (
         <div>
-            <span>About</span>
+            <div style={{ padding: '0px 2rem' }}>
+                Locate Us
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3783.4503678597025!2d73.84687707523656!3d18.508539382582626!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2c15a1c1b5211%3A0xb766cf8f496c68c8!2sNS%20COMPUTER%20HUB!5e0!3m2!1sen!2sin!4v1706781891229!5m2!1sen!2sin"
+                    width="100%"
+                    height="300"
+                    style={{ border: 0 }}
+                    loading="lazy"
+                    referrerPolicy="no-referrer-when-downgrade"
+                ></iframe>
+            </div>
         </div>
+
     )
 }
 
